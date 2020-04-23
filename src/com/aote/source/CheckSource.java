418d49c883f71a015553a987f47fdd0630534bd7
Weicf <332780922@qq.com>
2020-04-23 22:51:11 +0800

    source.

---
+++ src/com/aote/source/CheckSource.java
@@ -0,0 +1,18 @@
+package com.aote.source;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author aote
+ * @Date 2020-04-23 22:38
+ * @Version 1.0
+ * @Description TODO
+ **/
+public class CheckSource {
+
+    public static void main(String[] args) {
+        Map<String,String> map = new HashMap<>();
+    }
+
+}
